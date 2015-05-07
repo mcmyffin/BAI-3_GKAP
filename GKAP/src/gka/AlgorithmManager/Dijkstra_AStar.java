4cddd10b0ad9a999b03270355f2b35fd6942e7c8
sa-shan <saeed.shanidar@haw-hamburg.de>
2015-05-07 12:51:47 +0200

    doku abgabe version

--- GKAP/src/gka/AlgorithmManager/Dijkstra_AStar.java
+++ GKAP/src/gka/AlgorithmManager/Dijkstra_AStar.java
@@ -18,76 +18,76 @@ public class Dijkstra_AStar{
 	public static final String DIJKSTRA = "DIJKSTRA";
 	public static final String ASTAR 	= "ASTAR";
 	
-	private OwnVertex start;
-	private OwnVertex goal;
-	private Graph<OwnVertex,OwnEdge> graph;
-	private IAlgoReport reporter;
+	private OwnVertex _start;
+	private OwnVertex _goal;
+	private Graph<OwnVertex,OwnEdge> _graph;
+	private IAlgoReport _reporter;
 	
-	private Queue<OwnVertex> openList;
-	private Set<OwnVertex> closedList;
-	private Map<OwnVertex,OwnVertex> path;
-	private Map<OwnVertex, Integer> vertexLength;
+	private Queue<OwnVertex> _openList;
+	private Set<OwnVertex> _closedList;
+	private Map<OwnVertex,OwnVertex> _path;
+	private Map<OwnVertex, Integer> _vertexLength;
 
 	
 	public Dijkstra_AStar(Graph<OwnVertex,OwnEdge> g, OwnVertex start, OwnVertex goal, 
 											IAlgoReport reporter, String algorithmType){
 		
-		this.graph 	  = g;
-		this.start 	  = start;
-		this.goal  	  = goal;
-		this.reporter = reporter;						
+		this._graph 	  = g;
+		this._start 	  = start;
+		this._goal  	  = goal;
+		this._reporter = reporter;						
 		
-		this.vertexLength = new HashMap<OwnVertex, Integer>();
-		this.path     	  = new HashMap<OwnVertex, OwnVertex>();
-		this.closedList   = new HashSet<OwnVertex>();
+		this._vertexLength = new HashMap<OwnVertex, Integer>();
+		this._path     	  = new HashMap<OwnVertex, OwnVertex>();
+		this._closedList   = new HashSet<OwnVertex>();
 		
 		if(algorithmType.equals(DIJKSTRA))
 		{
-			this.openList 	= new PriorityQueue<OwnVertex>(new ComparatorLength(this.vertexLength));
+			this._openList 	= new PriorityQueue<OwnVertex>(new ComparatorLength(this._vertexLength));
 		}
 		else
 		{
-			this.openList 	= new PriorityQueue<OwnVertex>(new ComparatorHeuristic(this.vertexLength));
+			this._openList 	= new PriorityQueue<OwnVertex>(new ComparatorHeuristic(this._vertexLength));
 		}
 	}
 	
 
 	void startAlgorithm(){
 
-		reporter.startTimer();
+		_reporter.startTimer();
 		
 		// if the precondition fails, then interrupt procedure
-		if(defaultPrecondition(graph, start, goal)) return;
+		if(defaultPrecondition(_graph, _start, _goal)) return;
 
 		// if algorithm find target Vertex then calculate Path
 		if(findTarget()){
 			
 			// The target Vertex is a part of path
-			reporter.addPathNode(goal);
-			reporter.setPathLength(vertexLength.get(goal));
+			_reporter.addPathNode(_goal);
+			_reporter.setPathLength(_vertexLength.get(_goal));
 			
 			// Set up Pointer for loop
-			OwnVertex currentV = goal;
+			OwnVertex currentV = _goal;
 			
 			// Repeat until pointer "currentV" equals "start" Vertex
-			while(!currentV.equals(start)){
+			while(!currentV.equals(_start)){
 			
 				// save Successor pointer instartASternchen "tmpV" variable
-				OwnVertex tmpV = path.get(currentV);
+				OwnVertex tmpV = _path.get(currentV);
 				
 				// then add the Successor as part of Path in reporter Class 
-				reporter.addPathNode(tmpV);
+				_reporter.addPathNode(tmpV);
 				
 				// set new Pointer for next loop repeat
 				currentV = tmpV;
 			}
 			
 			// add visited Vertices to Reporter Class
-			for(OwnVertex v : vertexLength.keySet()){
-				reporter.addVisitedNode(v);
+			for(OwnVertex v : _vertexLength.keySet()){
+				_reporter.addVisitedNode(v);
 			}
 		}
-		reporter.stopTimer();
+		_reporter.stopTimer();
 	}
 	
 	/**
@@ -97,22 +97,22 @@ public class Dijkstra_AStar{
 	private boolean findTarget(){
 
 		// add start Vertex to priority Queue
-		vertexLength.put(start, 0);
-		openList.offer(start);
+		_vertexLength.put(_start, 0);
+		_openList.offer(_start);
 		
 		// Start Vertex is the predecessor to himself
-		path.put(start, start);
+		_path.put(_start, _start);
 		
-		while(!openList.isEmpty()){
+		while(!_openList.isEmpty()){
 			
 			// Get Vertex with lowest priority
-			OwnVertex currentVertex = openList.poll();
+			OwnVertex currentVertex = _openList.poll();
 			
 			// Set current Vertex as closed
-			closedList.add(currentVertex);
+			_closedList.add(currentVertex);
 			
 			// target found
-			if(goal.equals(currentVertex)) return true;
+			if(_goal.equals(currentVertex)) return true;
 			
 			// If target not found
 			// look at child Vertices and add these to openList
@@ -127,25 +127,25 @@ public class Dijkstra_AStar{
 	 */
 	private void expandNode(OwnVertex currentNode){
 		
-		for(OwnVertex successor : graph.getSuccessors(currentNode)){
-			reporter.countGraphAccess();
+		for(OwnVertex successor : _graph.getSuccessors(currentNode)){
+			_reporter.countGraphAccess();
 			// if successor Vertex in closed list, then do nothing
-			if(closedList.contains(successor)) continue;
+			if(_closedList.contains(successor)) continue;
 			
 			// calculate temporary path value
 			int tentative_g = getPathWeight(currentNode, successor);
 			
 			// if successor already in openList and the new pathValue >= current pathValue, then do nothing
-			if(openList.contains(successor) && tentative_g >= vertexLength.get(successor)) continue;
+			if(_openList.contains(successor) && tentative_g >= _vertexLength.get(successor)) continue;
 				
 			// Set new Predecessor
-			path.put(successor, currentNode);
+			_path.put(successor, currentNode);
 			
 			// set new path value
-			vertexLength.put(successor, tentative_g);
+			_vertexLength.put(successor, tentative_g);
 			
 			// add Child Vertex to openList if not in OpenList
-			if(!openList.contains(successor)) openList.offer(successor);
+			if(!_openList.contains(successor)) _openList.offer(successor);
 		}
 	}
 	
@@ -154,7 +154,7 @@ public class Dijkstra_AStar{
 	
 	private int getPathWeight(OwnVertex currentNode, OwnVertex successor){
 		
-		int path = vertexLength.get(currentNode) + graph.findEdge(currentNode, successor).getWeight();
+		int path = _vertexLength.get(currentNode) + _graph.findEdge(currentNode, successor).getWeight();
 		return path;
 	}
 	
